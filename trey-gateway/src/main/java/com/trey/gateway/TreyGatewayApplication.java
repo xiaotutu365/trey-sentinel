3dbf1d8dfc0f88084fa30b05fb0bb4ddb66d872a
turui <550228957@qq.com>
2020-07-10 21:34:15 +0800

    add gateway.

---
+++ trey-gateway/src/main/java/com/trey/gateway/TreyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.trey.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreyGatewayApplication.class, args);
+    }
+
+}
