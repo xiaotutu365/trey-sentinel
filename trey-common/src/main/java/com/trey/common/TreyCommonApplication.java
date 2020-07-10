c3ccd9170d9de4671abb76967874951b1355c79f
turui <550228957@qq.com>
2020-07-10 21:43:38 +0800

    add common.

---
+++ trey-common/src/main/java/com/trey/common/TreyCommonApplication.java
@@ -0,0 +1,13 @@
+package com.trey.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreyCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreyCommonApplication.class, args);
+    }
+
+}
